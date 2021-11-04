65324610a6360dab562fdf590c8f5f3a9857a365
szu-lisili <912824958@qq.com>
2021-11-04 19:56:03 +0800

    test

--- test.py
+++ test.py
@@ -1,2 +1,25 @@
+import tvm
+import tvm.testing
+from tvm import te
+import numpy as np
+
+tgt = tvm.target.Target(target="llvm", host="llvm")
+
+n = te.var("n")
+A = te.placeholder((n,), name="A")
+B = te.placeholder((n,), name="B")
+C = te.compute(A.shape, lambda i: A[i] + B[i], name="C")
+
+fadd = tvm.build(s, [A, B, C], tgt, name="myadd")
+
+dev = tvm.device(tgt.kind.name, 0)
+
+n = 1024
+a = tvm.nd.array(np.random.uniform(size=n).astype(A.dtype), dev)
+b = tvm.nd.array(np.random.uniform(size=n).astype(B.dtype), dev)
+c = tvm.nd.array(np.zeros(n, dtype=C.dtype), dev)
+fadd(a, b, c)
+tvm.testing.assert_allclose(c.numpy(), a.numpy() + b.numpy())
+
 if __name__ == '__main__':
     print("test")
