42571f54cdb7dae54399e975872c5f86e9bbeead
szu-lisili <912824958@qq.com>
2021-11-05 23:55:33 +0800

    test

--- test.py
+++ test.py
@@ -17,8 +17,9 @@ x_np = np.random.normal(size=[1, 224, 224, 32]).astype('int16')
 #y = relay.nn.conv2d(x, w, data_layout='NHWC', kernel_size=[3, 3], kernel_layout='OIHW')
 bpw = relay.nn.bitpack(w, bit_axis=2, pack_axis=2, bits=1, pack_type='uint8')
 #bpw = relay.nn.bitpack(w, bits=1, pack_axis=2, bit_axis=4, pack_type='uint8')
-#y = relay.nn.bitserial_conv2d(x, bpw, data_layout='NHWC', kernel_size=[1, 1], channels=32, pack_dtype='uint8', kernel_layout='HWIO')
-y = relay.nn.conv2d(x, w, data_layout='NHWC', kernel_size=[1, 1], channels=32, kernel_layout='HWIO')
+y = relay.nn.bitserial_conv2d(x, bpw, data_layout='NHWC', kernel_size=[1, 1], channels=32, pack_dtype='uint8', kernel_layout='HWIO')
+#y = relay.nn.conv2d(x, w, data_layout='NHWC', kernel_size=[1, 1], channels=32, kernel_layout='HWIO')
+
 y_func = relay.Function([x, w], y)
 
 print(y_func.body)
